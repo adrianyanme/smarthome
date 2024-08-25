f39ab2bf6f49d478c6110192eeea6eded9d613ad
adrianyanme <nusa.adrian.20@student.se.ittelkom-sby.ac.id>
2024-08-25 18:17:38 +0700

    Update bug hide and show icon

--- lib/controller/LoginController.dart
+++ lib/controller/LoginController.dart
@@ -1,41 +1,42 @@
-   import 'package:get/get.dart';
-   import 'package:dio/dio.dart';
-   import 'package:flutter/material.dart';
-   import 'package:shared_preferences/shared_preferences.dart';
+import 'package:get/get.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-   class LoginController extends GetxController {
-     var isLoading = false.obs;
-     var loginController = TextEditingController();
-     var passwordController = TextEditingController();
+class LoginController extends GetxController {
+  var isLoading = false.obs;
+  var loginController = TextEditingController();
+  var passwordController = TextEditingController();
+  var isPasswordVisible = false.obs; // Added this line
 
-     Future<void> login() async {
-       isLoading.value = true;
+  Future<void> login() async {
+    isLoading.value = true;
 
-       try {
-         var response = await Dio().post(
-           'http://baha.adrianyan.tech:8000/api/login',
-           data: {
-             'login': loginController.text,
-             'password': passwordController.text,
-           },
-         );
+    try {
+      var response = await Dio().post(
+        'http://baha.adrianyan.tech:8000/api/login',
+        data: {
+          'login': loginController.text,
+          'password': passwordController.text,
+        },
+      );
 
-         isLoading.value = false;
+      isLoading.value = false;
 
-         if (response.statusCode == 200) {
-           // Simpan token atau informasi sesi
-           SharedPreferences prefs = await SharedPreferences.getInstance();
-           await prefs.setString('token', response.data.toString());
+      if (response.statusCode == 200) {
+        // Simpan token atau informasi sesi
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.setString('token', response.data.toString());
 
-           // Jika login berhasil, arahkan ke halaman utama
-           Get.toNamed('/home');
-         } else {
-           // Jika login gagal, tampilkan pesan error
-           Get.snackbar('Error', 'Login failed', snackPosition: SnackPosition.BOTTOM);
-         }
-       } catch (e) {
-         isLoading.value = false;
-         Get.snackbar('Error', 'Login failed', snackPosition: SnackPosition.BOTTOM);
-       }
-     }
-   }
+        // Jika login berhasil, arahkan ke halaman utama
+        Get.toNamed('/home');
+      } else {
+        // Jika login gagal, tampilkan pesan error
+        Get.snackbar('Error', 'Login failed', snackPosition: SnackPosition.BOTTOM);
+      }
+    } catch (e) {
+      isLoading.value = false;
+      Get.snackbar('Error', 'Login failed', snackPosition: SnackPosition.BOTTOM);
+    }
+  }
+}
